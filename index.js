f6b6a472e44fd3c937a35b7914019440f98d2db7
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2023-10-20 02:01:47 +0600

    card data delte code added

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8ywqwd.mongodb.net/?retryWrites=true&w=majority`;
 
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -41,7 +40,7 @@ async function run() {
             const resut = await cursor.toArray()
             res.send(resut)
         })
-
+        
         app.get('/product/:id', async(req, res) =>{
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
@@ -49,6 +48,7 @@ async function run() {
             res.send(result)
         })
         
+
         app.put('/product/:id', async(req, res) =>{
             const id = req.params.id;
             console.log(id)
@@ -73,6 +73,13 @@ async function run() {
         app.post('/card', async(req, res) =>{
             const carddata = req.body;
             const result = await cardcollection.insertOne(carddata)
+            res.send(result)
+        })
+
+        app.delete('/card/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = { _id : new ObjectId(id)}
+            const result = await cardcollection.deleteOne(query)
             res.send(result)
         })
 
